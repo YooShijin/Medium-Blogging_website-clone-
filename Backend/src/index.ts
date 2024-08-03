bde19082f5181c411279b19f1b2c91cab1ef467e
YooShijin <a7mitk000@gmail.com>
2024-08-04 04:11:03 +0530

    Initialized pooled db

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,25 +1,55 @@
 import { Hono } from "hono";
+import { jwt, sign } from "hono/jwt";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
-const app = new Hono();
-
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET_KEY: string;
+  };
+}>();
 app.get("/", (c) => {
-  return c.text("Hello Hono!");
+  return c.text("Backend is available");
 });
 
-// POST /api/v1/user/signup
-// POST /api/v1/user/signin
-// POST /api/v1/blog
-// PUT /api/v1/blog
-// GET /api/v1/blog/:id
-// GET /api/v1/blog/bulk
+app.post("/api/v1/user/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post("/api/v1/user/signup", (c) => {
-  // Implement signup logic here
-  return c.json({ message: "User signed up successfully!" });
+  const body = await c.req.json();
+  try {
+    const user = await prisma.user.create({
+      data: {
+        email: body.username,
+        password: body.password,
+      },
+    });
+    const jwt = await sign({ id: user.id }, c.env.JWT_SECRET_KEY);
+    return c.json({ jwt });
+  } catch (e) {
+    c.status(403);
+    return c.json({ error: "Error while signing up" });
+  }
 });
-app.post("/api/v1/user/signin", (c) => {
-  // Implement signin logic here
-  return c.json({ message: "User signed in successfully!" });
+app.post("/api/v1/user/signin", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env?.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+  const user = await prisma.user.findUnique({
+    where: {
+      email: body.email,
+    },
+  });
+  if (!user) {
+    c.status(403);
+    return c.json({ error: "Invalid email or password" });
+  }
+  const jwt = await sign({ id: user.id }, c.env.JWT_SECRET_KEY);
+  return c.json({ jwt });
 });
 
 app.post("/api/v1/blog", (c) => c.text("POST /"));
